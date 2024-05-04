b5983e621eb9a201d8e87c826b1510901b99ff53
riajulislam17 <riaj17021995@gmail.com>
2024-05-05 00:50:54 +0600

    merge from main

--- src/components/Common/Loader.tsx
+++ src/components/Common/Loader.tsx
@@ -4,7 +4,7 @@ function Loader() {
   return (
     <>
       <div className="flex items-center justify-center h-screen">
-        <img src="./assets/logo.jpg" alt="Loader" className="animate-pulse" />
+        <img src="https://cdn2.hubspot.net/hub/219329/file-2131270105-png/fuel-10.png" alt="Loader" className="animate-pulse" />
         {/* <Image
           src={logo}
           alt="logo"
