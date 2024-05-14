3f8c9329c8cbdb8079574a49b406570e4fce6c5b
Ashadul Islam <ashadulmridhaprog@gmail.com>
2024-05-14 09:32:55 +0600

    feat: create originative landing

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,63 +1,58 @@
-import React, { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useState } from "react";
 // import { useDispatch } from "react-redux";
-import { useRouter } from "next/router";
-import Sidebar from "../Common/Sidebar";
 import Footer from "../Common/Footer";
-import Loader from "../Common/Loader";
+import Sidebar from "../Common/Sidebar";
 // import { handleResource } from "@/utils/APIRequester";
 // import { userSliceData } from "../../../app/feature/userSlice";
 import Header from "../Common/Header";
-import { handleResource } from "@/utils/APIRequester";
-import { useDispatch } from "react-redux";
-import { userSliceData } from "../../../app/feature/adminSlice";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  const router = useRouter();
-  const dispatch = useDispatch();
-  const [loading, setLoading] = useState(false);
+  // const router = useRouter();
+  // const dispatch = useDispatch();
+  // const [loading, setLoading] = useState(false);
   const [mobileMenu, setMobileMenu] = useState(false);
 
-  const getProfile = async () => {
-    setLoading(true);
-    try {
-      const result = await handleResource({
-        method: "get",
-        endpoint: "auth/validate-token",
-        popupMessage: false,
-      });
+  // const getProfile = async () => {
+  //   setLoading(true);
+  //   try {
+  //     const result = await handleResource({
+  //       method: "get",
+  //       endpoint: "auth/validate-token",
+  //       popupMessage: false,
+  //     });
 
-      if (result) {
-        dispatch(
-          userSliceData({
-            id: result._id,
-            // first_name: result.first_name,
-            // last_name: result.last_name,
-            email: result.email,
-            // phone: result.phone,
-          })
-        );
-      } else {
-        router.push("/signin");
-      }
-    } catch (error) {
-      if (error) {
-        router.push("/signin");
-      }
-    }
-    setLoading(false);
-  };
+  //     if (result) {
+  //       dispatch(
+  //         userSliceData({
+  //           id: result._id,
+  //           // first_name: result.first_name,
+  //           // last_name: result.last_name,
+  //           email: result.email,
+  //           // phone: result.phone,
+  //         })
+  //       );
+  //     } else {
+  //       router.push("/signin");
+  //     }
+  //   } catch (error) {
+  //     if (error) {
+  //       router.push("/signin");
+  //     }
+  //   }
+  //   setLoading(false);
+  // };
 
-  useEffect(() => {
-    getProfile();
-  }, []);
+  // useEffect(() => {
+  //   getProfile();
+  // }, []);
 
-  if (loading) {
-    return <Loader />;
-  }
+  // if (loading) {
+  //   return <Loader />;
+  // }
 
   return (
     // <>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,25 @@
-import "../styles/globals.css";
 import { AppProps } from "next/app";
-import Head from "next/head";
-import Layout from "@/components/Layout";
-import Cookies from "js-cookie";
-import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { Provider } from "react-redux";
-import { store } from "../../app/store";
-import Loader from "@/components/Common/Loader";
+import { useState } from "react";
+import Hero from "../components/Originative";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const [loading, setLoading] = useState(true);
-  const [token, setToken] = useState("");
+  // const [token, setToken] = useState("");
 
-  useEffect(() => {
-    const tokenFromCookie = Cookies.get(
-      `${process.env.NEXT_PUBLIC_TOKEN_NAME}`
-    );
-    if (!tokenFromCookie) {
-      router.push("/signin");
-    } else {
-      setToken(tokenFromCookie);
-      setLoading(false);
-    }
-  }, [router, token]);
+  // useEffect(() => {
+  //   const tokenFromCookie = Cookies.get(
+  //     `${process.env.NEXT_PUBLIC_TOKEN_NAME}`
+  //   );
+  //   if (!tokenFromCookie) {
+  //     router.push("/signin");
+  //   } else {
+  //     setToken(tokenFromCookie);
+  //     setLoading(false);
+  //   }
+  // }, [router, token]);
 
   const pagesWithoutLayout = ["/signin"];
 
@@ -32,32 +27,39 @@ function MyApp({ Component, pageProps }: AppProps) {
     router.pathname.startsWith(route)
   );
 
-  return shouldRenderWithoutLayout ? (
+  return (
     <>
-      <Head>
-        <link rel="icon" href="/assets/favicon.png" />
-      </Head>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
+      <Hero />
     </>
-  ) : (
-    <>
-      <Head>
-        <link rel="icon" href="/assets/favicon.png" />
-      </Head>
-      {loading ? (
-        <div>
-          <Loader />
-        </div>
-      ) : (
-        <Provider store={store}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </Provider>
-      )}
-    </>
+    //   shouldRenderWithoutLayout && (
+    //     <>
+    //       <Head>
+    //         <link rel="icon" href="/assets/favicon.png" />
+    //       </Head>
+    //       <Provider store={store}>
+    //         <Component {...pageProps} />
+    //       </Provider>
+    //     </>
+    //   )
+    // );
+    // ) : (
+    //   <>
+    //     <Head>
+    //       <link rel="icon" href="/assets/favicon.png" />
+    //     </Head>
+    //     {loading ? (
+    //       <div>
+    //         <Loader />
+    //       </div>
+    //     ) : (
+    //       // <Provider store={store}>
+    //       <Layout>
+    //         <Component {...pageProps} />
+    //       </Layout>
+    //       // </Provider>
+    //     )}
+    //   </>
+    // );
   );
 }
 
